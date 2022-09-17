21a93f3a3deaa4042b44aac9a7cd9ac176dfd4a7
Cristian Fonseca <cfonsecacomas@gmail.com>
2022-09-17 20:11:21 -0300

    chore(boilerplate✨): installed robotsTxt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import robotsTxt from 'astro-robots-txt';
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch()]
+  integrations: [tailwind(), prefetch(), robotsTxt()]
 });
