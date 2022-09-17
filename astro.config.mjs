8dac4a71007f53362bb5eb5f688ce9b8db8e0593
Cristian Fonseca <cfonsecacomas@gmail.com>
2022-09-17 20:12:29 -0300

    chore(boilerplate✨): installed react library integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from 'astro-robots-txt';
 import prefetch from "@astrojs/prefetch";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch(), robotsTxt()]
+  integrations: [tailwind(), prefetch(), robotsTxt(), react()]
 });
