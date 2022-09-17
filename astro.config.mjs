e04aa5ee69e8358cc0ca65284d4a6fdade367335
Cristian Fonseca <cfonsecacomas@gmail.com>
2022-09-17 20:20:33 -0300

    chore(boilerplate⚡️): fixed error in astro.config and configure paths of typescript in compileroptions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   
-  site: process.env.DOMAIN_URL || '',
+  site: process.env.DOMAIN_URL,
   experimental: {
     integrations: true
   },
