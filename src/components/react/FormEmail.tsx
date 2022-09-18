a1ab575706901a25ddf1a7f07b547672fdfa39d3
Cristian Fonseca <cfonsecacomas@gmail.com>
2022-09-17 21:27:24 -0300

    refactor(pages): passed template of form from page to new component in react FormEmail

--- src/components/react/FormEmail.tsx
+++ src/components/react/FormEmail.tsx
@@ -1,5 +1,19 @@
 import type React from "react";
 
 export const FormEmail: React.FC = () => {
-  return <div>FormEmail</div>;
+  return (
+    <form className="flex gap-2.5">
+      <input
+        type="email"
+        className="w-full dark:text-white dark:bg-[#323232] h-full p-2 text-base border-[#ccc] rounded border "
+        placeholder="Enter email"
+      />
+      <button
+        className="px-2.5 bg-[#2196f3] text-white disabled:opacity-70 font-semibold text-sm rounded"
+        type="submit"
+      >
+        Suscribe
+      </button>
+    </form>
+  );
 };
