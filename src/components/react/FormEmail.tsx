e8d4e7e333b189b86501cdcf38400eaa09bf3184
Cristian Fonseca <cfonsecacomas@gmail.com>
2022-09-17 21:29:47 -0300

    feat(components): create logic for change state and save value of email input into state

--- src/components/react/FormEmail.tsx
+++ src/components/react/FormEmail.tsx
@@ -1,15 +1,25 @@
 import type React from "react";
-
+import { useState } from "react";
 export const FormEmail: React.FC = () => {
+  const [email, setEmail] = useState("");
+  function handleChangeEmail(event: React.FormEvent<HTMLInputElement>) {
+    setEmail(event.currentTarget.value);
+  }
+  function handleSubmitEmail(event: React.FormEvent) {
+    return;
+  }
   return (
-    <form className="flex gap-2.5">
+    <form onSubmit={handleSubmitEmail} className="flex gap-2.5">
       <input
         type="email"
-        className="w-full dark:text-white dark:bg-[#323232] h-full p-2 text-base border-[#ccc] rounded border "
+        value={email}
+        onChange={handleChangeEmail}
+        className="w-full dark:text-white dark:bg-[#323232] h-full p-2 text-base border-[#ccc] rounded-md border "
         placeholder="Enter email"
       />
       <button
         className="px-2.5 bg-[#2196f3] text-white disabled:opacity-70 font-semibold text-sm rounded"
+        disabled={email === ""}
         type="submit"
       >
         Suscribe
