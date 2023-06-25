334511e16d66e6167ef49ca974eeecf459e341cb
Cristian Fonseca <cfonsecacomas@gmail.com>
2023-06-25 05:23:09 -0300

    🔧 chore(gitignore): add .env.* and lock files to .gitignore to prevent sensitive data and lock files from being committed 🔨 refactor(astro.config.mjs): update import statements to use double quotes for consistency ⬆️ upgrade(package.json): update dependencies and devDependencies to their latest versions for improved features and bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import robotsTxt from 'astro-robots-txt';
+import robotsTxt from "astro-robots-txt";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  
-  site: process.env.DOMAIN_URL || 'https://new.cristianfonseca.dev',
-  experimental: {
-    integrations: true
-  },
-  integrations: [tailwind(), prefetch({
-    throttle: 3
-  }), robotsTxt(), react(), mdx()]
+  site: process.env.DOMAIN_URL || "https://new.cristianfonseca.dev",
+  integrations: [
+    tailwind(),
+    prefetch({
+      throttle: 3,
+    }),
+    robotsTxt(),
+    react(),
+    mdx(),
+  ],
 });
